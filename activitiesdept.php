f5c80d936b931b4ed22ec5a6f1fc5a6842997eb0
Zteveluke <52157482+Zteveluke@users.noreply.github.com>
2021-03-17 01:43:36 +0530

    nnqphp

---
+++ activitiesdept.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="index.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="index.ico">
+    <title>BBQ</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">BBQ NATION</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Gallery</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="aboutdepartment.php">Menu</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Quick Links
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="studentlogin.php">Student Login</a></li>
+                  <li><a class="dropdown-item" href="facultylogin.php">Staff login</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Photo Gallery</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="aboutus.php" tabindex="-1" aria-disabled="true">About Us</a>
+              </li>
+            </ul>
+            <ul class="list-unstyled" style="font-size: 20px;color: rgb(0, 0, 255);">
+              <li>Phone: +91 - 484 - 5858142</li>
+              <li>Fax: +91 - 484 - 6547167</li>
+          </ul>
+          </div>
+        </div>
+      </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="d-grid gap-2">
+                    <button class="btn btn-light" type="button"><a href="aboutdepartment.php" data-bs-toggle="tooltip" title="Some tooltip text!">Kids</a></button>
+                    <button class="btn btn-light" type="button"><a href="associationdept.php" data-bs-toggle="tooltip" title="Some tooltip text!">Vegetarian</button>
+                    <button class="btn btn-light" type="button"><a href="activitiesdept.php" data-bs-toggle="tooltip" title="Some tooltip text!">Non-Vegetarian</a></button>
+                  </div>
+            </div>
+            <div class="col col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
+                <h1><b><u><center>Non-Vegetarian</center></u></b></h1>
+                <div class="accordion-body show" style="display: block;"><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1591602479-Dal Tadka-min.png" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1591602479-Dal Tadka-min.png"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Dal Tadka</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">Dal Tadka</div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607088678-veg.jpg" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607088678-veg.jpg"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Subj Mushkan</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">It’s a kolapuri style gravy with mix vegetable.</div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1590771808-Paneer Butter masala-min.png" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1590771808-Paneer Butter masala-min.png"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Paneer Butter Masala</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">A near perfect combination of spiciness and creaminess. This  popular punjabi dish is cooked with marinated paneer, fresh cream and spices. </div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607088889-index.jpg" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607088889-index.jpg"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Makhmali Kofta curry</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">It’s a popular dish made from banana dumpling cooked in a creamy onion tomato gravy.</div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1591097964-Steam Rice-min.png" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1591097964-Steam Rice-min.png"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Plain Rice</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">A versatile staple to many cuisines and a great accompaniment to any and every meal.</div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607089200-Aloo-Gobi-1-3.jpg" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607089200-Aloo-Gobi-1-3.jpg"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Aloo Gobi Tamater</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">Aloo gobi&nbsp;is a tradition Bengali&nbsp;vegetarian dish&nbsp;&nbsp;made with&nbsp;potatoes,&nbsp;cauliflowerand&nbsp;Indian spices.</div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607089407-Screenshot-23-Copy.jpg" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607089407-Screenshot-23-Copy.jpg"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Exotic veg in Coriander sauce</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">Veg is a Indo chinese dish of fried veggie in a spicy and tangy sauce.</div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1591098171-Tomato Rasam-min.png" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1591098171-Tomato Rasam-min.png"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Tomato Rasam</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">The South Indian style tomato rasam is full of flavors and tastes slightly tangy, spicy &amp; sweet.</div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607090277-index.jpg" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1607090277-index.jpg"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Chilli Garlic Noodles</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">Chilli Garlic Noodles is a spicy, Indian Chinese style noodles stir-fry with loads. </div></div></div><div class="menu__item__card"><div class="menu__item__image"><img alt="" data-src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1584189923-Kabuli Chana Biryani.jpg" class=" ls-is-cached lazyloaded" src="https://d2je4r3q8ihdyk.cloudfront.net/menu/item/1584189923-Kabuli Chana Biryani.jpg"></div><div class="menu__item__content"><div class="menu__item__title"><div class="menu__item__category veg"></div><div class="menu__item__name">Subj dum biryani</div><div class="menu__item__price">₹1.0</div></div><div class="menu__item__details">Vegetable Dum Biryani is very popular and rich rice preparation made with vegetables and cooked on dum to get flavourful and aromati.</div></div></div></div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="index.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="index.ico">
+    <title>BBQ</title>
+</head>
+<body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">BBQ NATION</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="gallery.php">Gallery</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="aboutdepartment.php">Menu</a>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Quick Links
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="studentlogin.php">Customer Login</a></li>
+                      <li><a class="dropdown-item" href="facultylogin.php">Staff login</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="gallery.php">Photo Gallery</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="aboutus.php" tabindex="-1" aria-disabled="true">About Us</a>
+                  </li>
+                </ul>
+                <ul class="list-unstyled" style="font-size: 20px;color: rgb(0, 0, 255);">
+                  <li>Phone: +91 - 484 - 5858142</li>
+                  <li>Fax: +91 - 484 - 6547167</li>
+              </ul>
+              </div>
+            </div>
+          </nav>
+          <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+              <span class="navbar-text">
+                <div class="container">
+                    <div class="row">
+                        <div class="col col-12 col-sm-2 col-lg-2 col-md-2 col-xl-2 col-xxl-2">
+                            <img height="10px" src="logo.svg" class="img-thumbnail" alt="...">
+                        </div>
+                        <div class="col col-12 col-sm-10 col-lg-10 col-md-10 col-xl-10 col-xxl-10">
+                            <strong st>Vision: </strong>At BBQ Nation it’s not only about eating.
+            <br><strong>Mission: </strong>Savor the moments, not the calories.
+                        </div>
+                    </div>
+                </div>
+              </span>
+            </div>
+          </nav>
+        </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#" style="font-family: Georgia, 'Times New Roman', Times, serif;">NEW UPDATES  : <MARQUEE style="font-family: Georgia, 'Times New Roman', Times, serif;;letter-spacing: 5.20px;" marquee scrollamount=12>
+              Download our Mobile App and avail Rs.100 off on your first booking & get 5% cashback on every transaction.</MARQUEE>  </a>
+            </div>
+          </div>
+        </nav>      
+    <div class="container">
+        <div class="row" >
+            <div class="col col-12 col-sm-12 col-md-12"></div>
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img height="700px" src="1.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img height="700px" src="2.jpg " class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img height="700px" src="3.jpg" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+        </div>
+        <div class="row">
+          <video autoplay="" muted="" loop="" id="banner-video"> <source src="https://d23ynomj6u3eig.cloudfront.net/sites/default/files/2021-03/Barbeque%20Nation%20-%20%231%20buffet%20grill%20barbeque%20restaurant%20near%20you-%20%281%29.mp4" type="video/mp4"> Your browser does not support the video tag. </video>
+            <div class="col col-12 col-sm-6 col-lg-6 col-md-6 col-xl-6 col-xxl-6">
+                <h2><center><u>Upcoming Events</u></center></h2>
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                          <b>  Weight loss Vs. Fat Loss – What’s the façade?
+                            Date: 26/Mar/2021 - 26/Mar/2021</b>
+                        </button>
+                      </h2>
+                      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <strong>About the Conference: </strong>About 30% of the world’s population is obese today, which includes children and adults of all ages. A significant number of people are dealing with obesity and are at a higher risk of developing a lifestyle disorder.
+                          Date: 26/Mar/2021 - 26/Mar/2021
+                          Time: 9:00 Am to 5:00 PM
+                        </div>
+                      </div>
+                    </div>
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                           <b> Grow, Nourish, Sustain Through Food!
+                            Date: 09/Apr/2021 - 10/Apr/2021</b>
+                        </button>
+                      </h2>
+                      <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <strong>About the Conference: </strong> It’s said the basic need of the individual is roti (food), kapda (body) and makaan (shelter). Give a deep thought to this, you will be amazed that food influences the clothes size that we wear and the kind of sleep that we get each day!.
+                          Date: 09/Apr/2021 - 10/Apr/2021
+                          Time: 9:00 Am to 5:00 PM
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+            <div class="col col-12 col-sm-6 col-lg-6 col-md-6 col-xl-6 col-xxl-6">
+                <h3>
+                   <center>Welcome To BBQ Nation</center> 
+                </h3>
+                <center><img height="30px" src="logo.svg" class="img-fluid" alt="..."></center>
+                <p></p>
+                Barbeque Nation was founded in 2006 by one of our Promoters. We currently own and operate 138 outlets in India, 5 outlets in UAE, 1 outlet in Malaysia and 1 outlet in Oman.
+
+One of the leading casual dining chains in India, Barbeque Nation pioneered the concept of “over the table barbeque” live grills embedded in dining tables – allowing guests to grill their own barbecue’s right at their tables.
+
+Indian cuisine is a natural part of the diet in the markets in which the Barbeque Nation Restaurants operate. We continue to offer new menu options, both vegetarian and non-vegetarian, largely based on Indian cuisine and use seasonal customer preferences to introduce new dishes. We also run popular food festivals at our Barbeque Nation Restaurants, thereby offering our customers a range of Indian and international cuisines.
+
+Our fixed price ‘all you can eat’ concept at our Barbeque Nation Restaurants offers our customers a varying spread of consistently delicious food at a value-oriented price in a pleasant and casual dining environment. We believe that barbeque cuisine is distinctively attractive as compared with other dining options due to its DIY and cooked-at-the-table nature and the flexibility and fun of mixing and matching a wide variety of meats, vegetables, sauces and condiments to create dishes according to each diner’s individual tastes and preferences. In addition, fixed price dining is very popular amongst relatively larger groups, due to customers’ perception of value for money and the comfort of certainty over the bill amount irrespective of varying individual appetites and consumption
+
+Our Barbeque Nation Restaurants typically offer customers at least five vegetarian and five non-vegetarian largely pre-cooked appetisers they can season and barbeque on a grill embedded in their table, an all-you-can-eat main course buffet and a selection of desserts.
+
+Our core values prioritise being customer-focused to provide excellent products and service and employee-focused by maintaining a superior quality work environment. Our Company was ranked 13th among best companies to work for in India across all categories by Economic Times “Great Places to Work” survey for 2019 conducted by Great Places to Work Institute. Our Company was also voted among the top ten employers within the Indian retail industry for 2019 by Great Places to Work and 14th among best large workplaces in Asia by Great Places to Work Institute.</p>
+            </div>
+        </div>
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>    
+</body>
+</html>
